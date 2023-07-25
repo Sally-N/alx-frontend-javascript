1877c7be50eebde07f9fbe4065040f1baf049aac
Sally-N <snzungula@gmail.com>
2023-07-25 05:14:34 +0000

    changes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];

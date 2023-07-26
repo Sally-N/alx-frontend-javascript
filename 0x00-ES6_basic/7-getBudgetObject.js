afa96fd3701e77bcf27ed166576672334cc466c3
Sally-N <snzungula@gmail.com>
2023-07-26 18:11:39 +0000

    changes

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 
 function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita
+  };
+
   return budget;
 }
 

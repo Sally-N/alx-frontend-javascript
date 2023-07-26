d56f7188de8254690cfd9ca666653c40d2038135
Sally-N <snzungula@gmail.com>
2023-07-26 19:26:26 +0000

    return object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,5 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = [...employeesList];
+  const allEmployees = { ...employeesList};
   
   const reportObj = {
     allEmployees,

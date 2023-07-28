42fd993c24786687ad87b44262a426466bdd2dc1
Sally-N <snzungula@gmail.com>
2023-07-28 00:37:38 +0000

    changes

--- 0x01-ES6_promise/7-load_balancer.js
+++
@@ -1,6 +0,0 @@
-function loadBalancer(chinaDownload, USDownload) {
-  // The Promise.race() takes an iterable of promises as input and returns a single Promise
-  return Promise.race([chinaDownload, USDownload]);
-}
-
-export default loadBalancer;

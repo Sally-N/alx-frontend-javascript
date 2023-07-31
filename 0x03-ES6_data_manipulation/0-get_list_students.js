77b6d57ac9df93824a4f2aee85b35a3bdd9e7524
Sally-N <snzungula@gmail.com>
2023-07-31 09:22:30 +0000

    array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export function getListStudents () {
+  let arrObj = [
+    {
+      "id": 1,
+      "firstName": "Guillame",
+      "location": "San Francisco"
+    },
+    {
+      "id": 2,
+      "firstName": "James",
+      "location": "Columbia"
+    },
+    {
+      "id": 5,
+      "firstName": "Serena",
+      "location": "San Francisco"
+    }
+  ]
+
+  return arrObj;
+}

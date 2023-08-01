e4bf3b6703443b1aafb71b2c39de3018a4a87f43
Sally-N <snzungula@gmail.com>
2023-08-01 08:27:23 +0000

    filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(studentList, city) {
-  return studentList.filter((student) => studentList.location == city);  
+  return studentList.filter((student) => student.location === city);  
 }
